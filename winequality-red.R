bf7c32060d2ff5b0d52f8155d53e04d694ce6794
greigar <greigar@gmail.com>
2017-11-29 17:35:51 +1300

    WIP

--- winequality-red.R
+++ winequality-red.R
@@ -37,6 +37,11 @@ predict_test <- function(model, reference_col_name, reference_col_num) {
   confusionMatrix(round(tab_predict), eval(parse(text = reference_col_name)))
 }
 
+varimp_results <- function(model) {
+  varimp_temp <- varImp(tab_model_rf)
+  tibble(name = rownames(varimp_temp), value = varimp_temp$Overall ) %>% arrange(desc(value))
+}
+
 stop("here be dragons")
 
 #
@@ -46,17 +51,36 @@ inTrain      <- createDataPartition(y = wine_data$quality, p = 0.75, list = FALS
 tab_training <- wine_data[inTrain,]
 tab_testing  <- wine_data[-inTrain,]
 
-tab_model_rf   <- randomForest(quality ~ ., data = as.data.frame(tab_training), importance = TRUE) # 0.664
+tab_model_rf <- randomForest(quality ~ ., data = as.data.frame(tab_training), importance = TRUE) # 0.664
+varimp_results(tab_model_rf)
+predict_test(tab_model_rf, "tab_testing$quality", 12)
 
-varimp_results <- varImp(tab_model_rf)
-tibble( name = rownames(varimp_results), value = varimp_results$Overall ) %>% arrange(desc(value))
+lm_model_1 <- lm(quality ~ alcohol, wine_data)
+lm_model_2 <- lm(quality ~ alcohol + volatile.acidity, tab_training)
+lm_model_3 <- lm(quality ~ alcohol + volatile.acidity + free.sulfur.dioxide, tab_training)
 
-predict_test_colour( tab_model_rf, "tab_testing$quality", 12)
-
-model_1 <- lm(quality ~ alcohol, wine_data)
-model_3 <- lm(quality ~ alcohol + volatile.acidity + free.sulfur.dioxide, wine_data)
-anova(model_1, model_3)
+anova(lm_model_1, lm_model_3)
 ggplot(wine_data, aes(x = alcohol, y = quality)) + geom_point() + geom_smooth(method = lm, formula = y ~ splines::ns(x, 7) )
+
+my_wine <- data.frame(alcohol = c(10.5), volatile.acidity = c(0.34))
+my_wine$quality <- round(predict(lm_model_2, newdata <- my_wine))
+
+g <- ggplot(wine_data, aes(x = alcohol, y = quality, colour = volatile.acidity)) +
+        geom_point() +
+        geom_smooth(method = lm, formula = y ~ splines::ns(x, 5) )
+g + geom_point(data= my_wine, aes(x = alcohol, y = quality, colour = volatile.acidity), fill = "white", shape = 21, size = 2)
+
+# make colour guess on wine type
+# use better model and then just plot lm
+g + geom_point(data= my_wine, aes(x = alcohol, y = quality, fill = volatile.acidity), colour = "red", shape = 21, stroke = 2, size = 4)
+
+
+my_wine <- data.frame(alcohol = c(14), volatile.acidity = c(0.54))
+my_wine$quality <- round(predict(lm_model_2, newdata <- my_wine))
+g + geom_point(data= my_wine, aes(x = alcohol, y = quality, colour = volatile.acidity), fill = "white", stroke = 2, shape = 21, size = 2)
+
+
+
 
 #tab_model_crf <- train(quality ~ ., data = tab_training, method = "rf")        # 0.668
 #tab_model_gbm <- train(quality ~ ., data = tab_training, method = "gbm")       # 0.576
